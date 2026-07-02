1e37ba4f193d16eb853b1fbf93f9c8bfd9b07ab9
Michal Málek <nagahelmic@seznam.cz>
2026-07-02 21:14:14 +0200

    UPDATE: Display Work

--- lib/Display/Display.cpp
+++ lib/Display/Display.cpp
@@ -1,8 +1,24 @@
 #include "Display.h"
 #include <Arduino.h>
+#include <Wire.h>
+#include "Pins.h"
 
 void Display::begin()
 {
+    Wire.begin(Pins::Display::Sda, Pins::Display::Scl);
+
+    if (!oled.begin(0x3C, true)) {
+        Serial.println("OLED initialization failed");
+        return;
+    }
+
+    oled.clearDisplay();
+    oled.setTextColor(SH110X_WHITE);
+    oled.setTextSize(1);
+    oled.setCursor(0, 0);
+    oled.println("Display OK");
+    oled.display();
+
     Serial.println("Display module initialized");
 }
 
@@ -11,7 +27,21 @@ void Display::update()
     // Display update logic will be added later.
 }
 
-void Display::render()
-{
+void Display::render() {
+    
+    oled.clearDisplay();
+
+    oled.setTextSize(1);
+    oled.setCursor(0, 0);
+    oled.println("AquariumController");
+
+    oled.setCursor(0, 16);
+    oled.println("Display test");
+
+    oled.setCursor(0, 32);
+    oled.println("OLED SH1106 OK");
+
+    oled.display();
+
     Serial.println("Display render called");
 }
--- lib/Display/Display.h
+++ lib/Display/Display.h
@@ -1,8 +1,12 @@
 #pragma
+#include <Adafruit_SH110X.h>
 
 class Display {
     public:
         void begin();
         void update();
         void render();
+        
+    private:
+    Adafruit_SH1106G oled = Adafruit_SH1106G(128, 64, &Wire, -1);
 };
